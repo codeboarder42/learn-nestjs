8a811275cf01b7983af26f34b3c9d34ceb299a04
Fabien Quillévéré <quillevere.fabien@gmail.com>
2025-05-15 13:44:50 +0200

    test: following previous commit

--- src/dog/dog.service.spec.ts
+++ src/dog/dog.service.spec.ts
@@ -5,14 +5,18 @@ describe('DogService', () => {
   let service: DogService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [DogService],
-    }).compile();
-
-    service = module.get<DogService>(DogService);
+    service = new DogService();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of dogs', async () => {
+      const results = ['Oly', 'Piply', 'Ely'];
+      const expectedResult = await service.findAll();
+      expect(expectedResult).toStrictEqual(results);
+    });
   });
 });
--- src/dog/dog.service.ts
+++ src/dog/dog.service.ts
@@ -1,20 +1,10 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ALL_DOG } from './bdd';
-import { ModuleRef } from '@nestjs/core';
-import { ConfigType } from '@nestjs/config';
-import { BirdService } from 'src/bird/bird.service';
-import database from 'src/environement/database';
 
 @Injectable()
 export class DogService {
-  constructor(
-    private readonly moduleRef: ModuleRef,
-    @Inject(database.KEY)
-    private dbConfig: ConfigType<typeof database>,
-  ) {}
+  constructor() {}
   findAll(): Promise<string[]> {
-    const birdService = this.moduleRef.create(BirdService);
-    const databaseHost = this.dbConfig.host;
     return Promise.resolve(ALL_DOG);
   }
 }
